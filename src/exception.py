800cb97b72f17ba1ccc636c89282cacc81a7c11e
abhinav200501 <abhinav200501@gmail.com>
2026-03-14 13:45:51 +0530

    DataIngestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
-import logging
+from src.logger import logging
+
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -10,7 +11,7 @@ def error_message_detail(error,error_detail:sys):
     return error_message
 
 
-class CustomException(Exception):
+class VisibilityException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
